63b411bce7d3ceb088a32e1e9ad95a81487ea4f7
haoyalei <hao19903888974@outlook.com>
2025-03-11 00:47:02 +0800

    import neotree

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- 设置空格为leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- 开启neo-tree
+vim.keymap.set('n', '<leader>e', ':Neotree<CR>', { desc = 'Toggle NeoTree' })
+
+-- 切换焦点到 neo-tree
+vim.keymap.set('n', '<C-h>', ':Neotree focus<CR>', { desc = 'Focus on NeoTree' })
+
+-- 切换焦点到编辑区
+vim.keymap.set('n', '<C-l>', ':wincmd p<CR>', { desc = 'Focus on editor' })
